fd92b573450da45fd0127fa2843da17110b80435
cuong1979 <70330972+cuong1979@users.noreply.github.com>
2020-11-13 13:26:38 +0100

    hämtat alla från data

--- app.js
+++ app.js
@@ -3,4 +3,17 @@ let cityName = 'ängelholm'
 
 let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`
 
-console.log(url);
+fetch(url).then(
+    function(respsone){
+        return respsone.json();
+    }
+).then(
+    function(data){
+        //console.log(data);
+        let description = data.weather[0].description;
+        let icon = data.weather[0].icon;
+        let temperatur = data.main.temp;
+        let vindhastighet = data.wind.speed;
+        let luftfuktighet = data.main.humidity;
+    }
+)
