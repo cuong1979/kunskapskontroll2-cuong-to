89d50ecb8994b5daca21a579ed043874b63e3033
cuong1979 <70330972+cuong1979@users.noreply.github.com>
2020-11-14 09:36:31 +0100

    bakgrund bild

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 
-
-
+//hämta from och se skapa en click event 
 let form = document.querySelector('#vädret');
 form.addEventListener('click', function(event){
     event.preventDefault();
     
+// hämta input så när skriva in stad som man vill söka för
     let stadNameInput = document.querySelector('#stad');
-
     let cityName = stadNameInput.value;
 
-    let apiKey = '56e3472f920d06a692d2b0fd0ee40b76'
+    // här min API keys kod
+ let apiKey = '56e3472f920d06a692d2b0fd0ee40b76'
+ // här skapa jag add en cityname så man samma länka med väder app
  let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`
+
 
  fetch(url).then(
     function(respsone){
@@ -19,21 +21,25 @@ form.addEventListener('click', function(event){
 ).then(
 function(data){
     console.log(data);
+    // här hämta jag alla info jag behöver
     let description = data.weather[0].description;
     let icon = data.weather[0].icon;
     let temperatur = Math.round(data.main.temp);
     let vindhastighet = data.wind.speed;
     let luftfuktighet = data.main.humidity;
     
-    let beskriving = document.querySelector('.description');
-    let temp = document.querySelector('.temperatur');
-    let vind = document.querySelector('.vindhastighet');
-    let luftfuktigt = document.querySelector('.luftfuktighet');
-    
+    // här lägga jag in mina info så man kan läsa den webbläsare
+   let beskriving = document.querySelector('.description');
+   let temp = document.querySelector('.temperatur');
+   let vind = document.querySelector('.vindhastighet');
+   let luftfuktigt = document.querySelector('.luftfuktighet');
+
     beskriving.textContent = description;
     temp.textContent = temperatur;
     vind.textContent = vindhastighet;
     luftfuktigt.textContent = luftfuktighet;
+
+    
     
     
 }
