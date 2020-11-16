3d565cb6b9a913ca01edb327e8b2fd4bea22d8ca
cuong1979 <70330972+cuong1979@users.noreply.github.com>
2020-11-16 17:06:35 +0100

    bilder spara

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const apiKey = '56e3472f920d06a692d2b0fd0ee40b76';
 
 // skapa en variable till stad 
-//let cityName = 'ängelholm';
+let cityName;
 
 // hämtas alla elemnet
 let beskrivning = document.querySelector('h1');
@@ -11,72 +11,77 @@ let temp = document.querySelector('.temperature');
 let vind = document.querySelector('.vindshastigt');
 let luft = document.querySelector('.luftfuktighet');
 
-
-
     // hämta button från html för att event
-    let btn = document.querySelector('button')
-    // button bli min event med click
+    let btn = document.querySelector('button');
+     
+    // hämta input med class 
+     let stadNamninput = document.querySelector('.stad-input');
+     let body = document.querySelector('body');
+     // button bli min event med click
     btn.addEventListener('click', function(event){
     //  preventDefault gör att sök text i den fallen inte försvinner.
     event.preventDefault();
-    // hämta input med class 
-    let stadNamninput = document.querySelector('.stad-input');
-    // skapa variable till för att pusha upp i input fälten.
-    let cityName = stadNamninput.value;
    
-        // skapa en url till API och lägg in cityName variable och API key variable
+    // skapa variable till för att pusha upp i input fälten.
+    cityName = stadNamninput.value;
+    hämtaVäder();
+    
+});
+
+ function hämtaVäder(){
+    // skapa en url till API och lägg in cityName variable och API key variable
     const url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`;
 
-// fetch(url).then((response => response.json())).then(data => console.log(data));
-// hämta url/API gör om den till .json 
-fetch(url).then(function(response){
-        return response.json();
+    // fetch(url).then((response => response.json())).then(data => console.log(data));
+    // hämta url/API gör om den till .json 
+    fetch(url).then(function(response){
+    return response.json();
     // hämta alla data vi behöver och sortera dom
-}).then(function(data){
+    }).then(function(data){
     let beskrivningen = data.weather[0].description;
     let väderIcon = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
     let väderTemp = Math.round(data.main.temp);
     let väderVind = data.wind.speed;
     let väderluftfuktig = data.main.humidity;
-   
-// byt elemneter som vi har hämtat från data
-    beskrivning.innerText = beskrivningen;
-    imgIkon.src = väderIcon;
-    temp.innerText = väderTemp;
-    vind.innerText = väderVind;
-    luft.innerText = väderluftfuktig;
-    //console.log(väderluftfuktig);
-    changeColor(väderTemp);
 
-    }).catch(function(error) {
-        alert('Stad som du har sökt finns inte');
-    });
-    
-}) 
-let body = document.querySelector('body');
+// byt elemneter som vi har hämtat från data
+beskrivning.innerText = beskrivningen;
+imgIkon.src = väderIcon;
+temp.innerText = `${väderTemp} °C` ;
+vind.innerText =  `vindhastighet ${väderVind}` ;
+luft.innerText =  `luftfukighet ${väderluftfuktig}` ;
+//console.log(väderluftfuktig);
+changeColor(väderTemp);
+
+  }).catch(function() {
+    alert('Stad som du har sökt finns inte');
+});
+       
+};
+
+
+// hongkong och hawaii fungera inte 
+
+
 
 
 function changeColor(väderTemp) {
 
-    if (väderTemp <= -25) {
-        body.style.backgroundColor = 'red';
-    } else if (väderTemp <= -10 && väderTemp > -25 ) {
-        body.style.backgroundColor = 'blue';
-    } else if (väderTemp <= 0 && väderTemp > -10) {
-        body.style.backgroundColor = 'yellow';
-    } else if (väderTemp <= 5 && väderTemp > 0) {
-        body.style.backgroundColor = 'orange';
-    } else if (väderTemp <= 10 && väderTemp > 5) {
-        body.style.backgroundColor = 'navy';
-    } else if (väderTemp <= 20 && väderTemp > 10) {
-        body.style.backgroundColor = 'olive';
-    } else if (väderTemp <= 30 && weatherTemperatur > 40) {
-        body.style.backgroundColor = 'grey';
+    if (väderTemp <= -10) {
+        body.style.backgroundImage = "url('/Img/vinter-10.jpg')";
+    } else if (väderTemp <= 0) {
+        body.style.backgroundImage = "url('/Img/vinter0.jpg')";
+    } else if (väderTemp <= 5) {
+        body.style.backgroundImage = "url('/Img/snö5.jpg')";
+    } else if (väderTemp <= 10) {
+        body.style.backgroundImage = "url('/Img/vår10.jpg')";
+    } else if (väderTemp <= 20 && weatherTemperatur > 30) {
+        body.style.backgroundImage = "url('/Img/somma20.jpg')";
     } else {
-        body.style.backgroundColor = 'pink';
+        body.style.backgroundImage = "url('/Img/somma30.jpg')";
     }
 
-}
+};
 
 
 
